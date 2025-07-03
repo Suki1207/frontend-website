c66ed3adb542ed115762202b0fa069020ec9e0d9
Suada <selimovicsuki@gmail.com>
2025-07-03 03:28:46 +0200

    Create light-dark-toggle functionality

--- src/main.js
+++ src/main.js
@@ -1,2 +1,19 @@
 // AOS install
 AOS.init();
+
+// Light Dark Toggle
+function setTheme() {
+  const toggleBtnElement = document.querySelector(".light-dark-toggle");
+  const htmlElement = document.documentElement;
+
+  document.addEventListener("click", () => {
+    if (htmlElement.classList.contains("light-mode")) {
+      htmlElement.classList.remove("light-mode");
+      toggleBtnElement.innerHTML = `<i class='fa-solid fa-sun' ></i>`;
+    } else {
+      htmlElement.classList.add("light-mode");
+      toggleBtnElement.innerHTML = `<i class='fa-regular fa-moon' ></i>`;
+    }
+  });
+}
+setTheme();
