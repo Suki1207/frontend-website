96c53e1ab34f652c304887125a7fb617faf65370
Suada <selimovicsuki@gmail.com>
2025-07-04 18:46:43 +0200

    Create project filtering functionality

--- src/main.js
+++ src/main.js
@@ -20,24 +20,65 @@ setTheme();
 
 // Typing Effect
 function setTypingEffect() {
-  const roleElement = document.getElementById("role");
-  const roles = ["Frontend Developer"];
-  let letterIndex = 0;
+  document.addEventListener("DOMContentLoaded", () => {
+    const roleElement = document.getElementById("role");
+    const roles = ["Frontend Developer"];
+    let letterIndex = 0;
 
-  function typeRole() {
-    roleElement.textContent = roles[0].slice(0, letterIndex);
-    letterIndex++;
+    function typeRole() {
+      roleElement.textContent = roles[0].slice(0, letterIndex);
+      letterIndex++;
 
-    if (letterIndex > roles[0].length) {
-      setTimeout(() => {
-        letterIndex = 0;
-      }, 850);
+      if (letterIndex > roles[0].length) {
+        setTimeout(() => {
+          letterIndex = 0;
+        }, 850);
+      }
     }
-  }
 
-  setInterval(() => {
-    typeRole();
-  }, 100);
+    setInterval(() => {
+      typeRole();
+    }, 100);
+  });
 }
+setTypingEffect();
 
-document.addEventListener("DOMContentLoaded", setTypingEffect);
+// Project Filter
+function filterProjects() {
+  const filterBtnsElement = document.querySelectorAll(".project-list li");
+  const projectCardsElement = document.querySelectorAll(".project-box");
+
+  filterBtnsElement.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      filterBtnsElement.forEach((button) => {
+        button.classList.remove("active");
+      });
+      btn.classList.add("active");
+
+      const filterValue = btn.getAttribute("data-filter").trim().toLowerCase();
+
+      projectCardsElement.forEach((card) => {
+        const cardCategory = card
+          .getAttribute("data-category")
+          .trim()
+          .toLowerCase();
+
+        // Debugging
+        console.log(`Filter: "${filterValue}", Card: "${cardCategory}"`);
+
+        if (filterValue === "all" || cardCategory === filterValue) {
+          card.style.display = "flex";
+          card.setAttribute("data-aos", "fade-up");
+        } else {
+          card.style.display = "none";
+          card.removeAttribute("data-aos");
+        }
+      });
+
+      if (window.AOS) {
+        AOS.refresh();
+      }
+    });
+  });
+}
+filterProjects();
